dfc3dabc8fd68c2443490dc0f9c4d69e751ee2e0
6shay9 <zetushiomawa@gmail.com>
2026-02-16 16:38:07 +0800

    Complete all models: Organization, Student, OrgMember

--- studentorg/models.py
+++ studentorg/models.py
@@ -19,3 +19,26 @@ class Program(BaseModel):
 
     def __str__(self):
         return self.prog_name
+    
+class Organization(BaseModel):
+    name = models.CharField(max_length=250)
+    college = models.ForeignKey(College, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Student(BaseModel):
+    student_id = models.CharField(max_length=15, unique=True)
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class OrgMember(BaseModel):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    date_joined = models.DateField()
