26930a3deab3bfcdd9d2f7004648d78152f1a800
JM MAYORES <josemiguelmayores@gmail.com>
2026-02-16 17:33:22 +0800

    modified models.py

--- studentorg/models.py
+++ studentorg/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -9,34 +9,30 @@ class BaseModel(models.Model):
 
 class College(BaseModel):
     college_name = models.CharField(max_length=150)
-
     def __str__(self):
         return self.college_name
 
 class Program(BaseModel):
     prog_name = models.CharField(max_length=150)
     college = models.ForeignKey(College, on_delete=models.CASCADE)
-
     def __str__(self):
         return self.prog_name
-    
+
 class Organization(BaseModel):
     name = models.CharField(max_length=250)
-    college = models.ForeignKey(College, on_delete=models.CASCADE)
-    description = models.TextField()
-
+    college = models.ForeignKey(College, null=True, blank=True, on_delete=models.CASCADE)
+    description = models.CharField(max_length=500)
     def __str__(self):
         return self.name
 
 class Student(BaseModel):
-    student_id = models.CharField(max_length=15, unique=True)
-    last_name = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    student_id = models.CharField(max_length=15)
+    lastname = models.CharField(max_length=25)
+    firstname = models.CharField(max_length=25)
+    middlename = models.CharField(max_length=25, blank=True, null=True)
     program = models.ForeignKey(Program, on_delete=models.CASCADE)
-
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.lastname}, {self.firstname}"
 
 class OrgMember(BaseModel):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
