98b9a66c6c8e100df570b8ade157019c32e47dbb
JM MAYORES <josemiguelmayores@gmail.com>
2026-02-16 16:08:39 +0800

    Added College and Program models

--- studentorg/models.py
+++ studentorg/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class College(BaseModel):
+    college_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.college_name
+
+class Program(BaseModel):
+    prog_name = models.CharField(max_length=150)
+    college = models.ForeignKey(College, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.prog_name
