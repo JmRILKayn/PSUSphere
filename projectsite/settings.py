22c60a701b3adf2a42758eb38cd78b5b7aa0b198
JM MAYORES <josemiguelmayores@gmail.com>
2026-02-17 18:39:49 +0800

    modified settings.py

--- projectsite/settings.py
+++ projectsite/settings.py
@@ -71,10 +71,12 @@ TEMPLATES = [
 ]
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = (
-BASE_DIR / 'static',
-)
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 WSGI_APPLICATION = 'projectsite.wsgi.application'
 
