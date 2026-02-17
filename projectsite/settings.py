0c9cca84e2776111179154c63ff2a0202f075c50
JM MAYORES <josemiguelmayores@gmail.com>
2026-02-17 18:01:04 +0800

    Modified the settings.py

--- projectsite/settings.py
+++ projectsite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'projectsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],       
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +67,12 @@ TEMPLATES = [
         },
     },
 ]
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = (
+BASE_DIR / 'static',
+)
 
 WSGI_APPLICATION = 'projectsite.wsgi.application'
 
