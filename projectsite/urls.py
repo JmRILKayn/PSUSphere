4172628c49c6d22b71261f816d2e11ebe0e9c6f2
JM MAYORES <josemiguelmayores@gmail.com>
2026-02-17 18:16:35 +0800

    Modified the settings.py

--- projectsite/urls.py
+++ projectsite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from studentorg.views import HomePageView
 from studentorg import views
 
 urlpatterns = [
