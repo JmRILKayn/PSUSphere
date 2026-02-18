8fbac3819ba696ccb41bf293917bc37ef3a7310e
JM MAYORES <josemiguelmayores@gmail.com>
2026-02-18 12:09:05 +0800

    modify settings.py

--- projectsite/settings.py
+++ projectsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(#&^2)4a+!0x2d706h#e=znw6!j0o0eml7xfm0!d-9fzqanwb4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
